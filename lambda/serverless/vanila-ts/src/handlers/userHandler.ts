c059ec13ce814999c98903aa9463256cfed34641
Jayakumar Jayaraman <india.jai@gmail.com>
2022-06-03 09:47:15 +0100

    clean up

--- lambda/serverless/vanila-ts/src/handlers/userHandler.ts
+++ lambda/serverless/vanila-ts/src/handlers/userHandler.ts
@@ -1,5 +1,6 @@
 import AWS from 'aws-sdk'
-import APIGatewayProxyEvent from 'aws-lambda'
+import {APIGatewayProxyEvent} from 'aws-lambda'
+    
 
 const createUser = (event: APIGatewayProxyEvent) => {
 
