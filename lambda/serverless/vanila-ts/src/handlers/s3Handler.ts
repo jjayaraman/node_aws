b0b8493a1a5d580c7b0e8707f9e5b5d7d2c70b2c
Jayakumar Jayaraman <india.jai@gmail.com>
2022-06-07 22:29:37 +0100

    s3

--- lambda/serverless/vanila-ts/src/handlers/s3Handler.ts
+++ lambda/serverless/vanila-ts/src/handlers/s3Handler.ts
@@ -9,18 +9,22 @@ const s3Service = new S3Service(s3Client)
 
 export const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log('BUCKET_NAME : ', BUCKET_NAME)
-
+    let statusCode = 200
+    let body
     let data;
     try {
         data = await s3Service.listObjects(BUCKET_NAME)
         console.log('data loaded : ', data);
-
+        statusCode = 200
+        body = JSON.stringify(data)
     } catch (error) {
+        statusCode = 500
+        body = `Error loading buckets : ${error}`
         console.error('Error loading buckets : ', error);
     }
 
     return {
-        'statusCode': 200,
-        'body': JSON.stringify(data)
+        statusCode,
+        body
     }
 }
