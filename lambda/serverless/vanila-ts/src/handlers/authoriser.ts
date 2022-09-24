4d1f8741233c0affb59e8465c4ae76410b864cd9
Jayakumar Jayaraman <jjayaraman@onvutech.com>
2022-09-24 16:14:45 +0100

    throtling added

--- lambda/serverless/vanila-ts/src/handlers/authoriser.ts
+++ lambda/serverless/vanila-ts/src/handlers/authoriser.ts
@@ -7,17 +7,31 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  * @param event 
  * @returns 
  */
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = async (event: any): Promise<any> => {
 
     console.log('event :: ', JSON.stringify(event));
 
-    const result = {
-        'message': 'Hello authorizer'
+    const token = event.token
+    const methodArn = event.methodArn
+
+    let effect = 'deny'
+    if (token && token === 'jay') {
+        effect = 'allow'
     }
 
-    let respone: APIGatewayProxyResult = {
-        statusCode: 200,
-        body: JSON.stringify(result)
+    const policyDocument = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "execute-api:Invoke",
+                "Effect": effect,
+                "Resource": methodArn
+            }
+        ]
+    }
+
+    let respone = {
+        policyDocument
     }
 
     return respone
