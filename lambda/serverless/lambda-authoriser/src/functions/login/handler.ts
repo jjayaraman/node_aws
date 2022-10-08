07af76779da4011da312e039f4a049e77593a789
Jayakumar Jayaraman <jjayaraman@onvutech.com>
2022-10-08 13:07:04 +0100

    ts types added

--- lambda/serverless/lambda-authoriser/src/functions/login/handler.ts
+++ lambda/serverless/lambda-authoriser/src/functions/login/handler.ts
@@ -1,14 +1,39 @@
 
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { sign, SignOptions } from 'jsonwebtoken';
 
-export const login = (event: any) => {
+export const login = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
     console.log(`event : ${JSON.stringify(event)}`);
+    const body = JSON.parse(event.body as string);
 
-    const username = event.body.username || 'jay'
-    const password = event.body.password || 'jay'
+    const username = body?.username || 'jay'
+    const password = body?.password || 'jay'
 
-    // Dummy authentication
+    await authenticate(username, password)
+        .then(token => {
+            console.log(`token:  ${token}`);
+            return {
+                statusCode: 200,
+                body: { token }
+            };
+        })
+        .catch(error => {
+            return {
+                statusCode: 401,
+                body: `Invalid credentials. Please try again`
+            };
+        })
+
+    return {
+        statusCode: 401,
+        body: `Invalid credentials. Please try again`
+    };
+}
+
+
+// Dummy authentication
+const authenticate = async (username: string, password: string): Promise<string> => {
     if (username && password) {
         if (username === 'jay' && password === 'jay') {
             const payload = { username }
@@ -21,16 +46,8 @@ export const login = (event: any) => {
             }
 
             const token = sign(payload, SECRET, options);
-            console.log(`token:  ${token}`);
-            return {
-                statusCode: 200,
-                body: JSON.stringify(token)
-            };
+            return token;
         }
     }
-
-    return {
-        statusCode: 401,
-        body: `Invalid credentials. Please try again`
-    };
+    return Promise.reject('Invalid credentials')
 }
